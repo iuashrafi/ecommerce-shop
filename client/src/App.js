dad92f7720ecbe3bccb6f8e789ee60e9a8591455
Imtiaz Uddin <iuashrafi@gmail.com>
2023-05-11 05:19:57 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
+//Routes
+import Home from "./pages/home/Home";
+import ItemDetails from "./pages/itemDetails/ItemDetails";
+import Checkout from "./pages/checkout/Checkout";
+import Confirmation from "./pages/checkout/Confirmation";
+import Navbar from "./pages/global/Navbar";
+import CartMenu from "./pages/global/CartMenu";
+import Footer from "./pages/global/Footer";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
+        <CartMenu />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
